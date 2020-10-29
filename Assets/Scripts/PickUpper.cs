e5052e1837fb6abac5285c0bca52a71212d4b993
TimepieceMaster <JacobVillarrealTP@gmail.com>
2020-10-29 16:18:24 -0500

    Pickuppable objects are highlighted when looked at

--- Assets/Scripts/PickUpper.cs
+++ Assets/Scripts/PickUpper.cs
@@ -1,6 +1,7 @@
 ﻿using Gvr.Internal;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
 using UnityEngine;
 
 public class PickUpper : MonoBehaviour
@@ -9,6 +10,7 @@ public class PickUpper : MonoBehaviour
     public Transform holdingPosition;
     public float holdingDistance = 0f;
     private Rigidbody heldObject;
+    private GameObject currentPickupabbleLookingAt;
     private int onlyPickUppableObjectsMask = 1 << 8;
     private KeyCode pickUpButtonController = KeyCode.Joystick1Button0;
     private KeyCode pickUpButtonKeyboard = KeyCode.E;
@@ -20,6 +22,7 @@ public class PickUpper : MonoBehaviour
     private float rotateObjectSpeed = 120.0f;
     private float moveForwardBackwardSpeed = 1.0f;
     private float maxMoveForwardBackwardDistance = 1.5f;
+    private Color objectHighlightColor = Color.red;
 
     void AttemptPickup()
 	{
@@ -29,6 +32,13 @@ public class PickUpper : MonoBehaviour
 		{
             heldObject = raycastHit.rigidbody;
             heldObject.freezeRotation = true;
+
+            // make sure we unhighlight the object
+            if (currentPickupabbleLookingAt != null)
+			{
+                Material previousObjectMaterial = currentPickupabbleLookingAt.GetComponent<Renderer>().material;
+                previousObjectMaterial.color = Color.white;
+            }
         }
 	}
 
@@ -70,13 +80,67 @@ public class PickUpper : MonoBehaviour
         holdingDistance = 0;
 	}
 
+    void LookForPickuppableObjects()
+	{
+        RaycastHit raycastHit;
+        GameObject pickuppableObject;
+        Physics.Raycast(viewingPosition.position, viewingPosition.forward, out raycastHit, pickUpDistance, onlyPickUppableObjectsMask);
+        
+        // we're looking at a pickuppable object
+        if (raycastHit.rigidbody != null)
+        {
+            // we weren't previously looking at something
+            if (currentPickupabbleLookingAt == null)
+			{
+                // set object color to highlight color
+                pickuppableObject = raycastHit.transform.gameObject;
+                Material objectMaterial = pickuppableObject.GetComponent<Renderer>().material;
+                objectMaterial.color = objectHighlightColor;
+                currentPickupabbleLookingAt = pickuppableObject;
+            }
+            // we're looking at something new
+            else if (currentPickupabbleLookingAt != raycastHit.transform.gameObject)
+			{
+                // set new object color to highlight color
+                pickuppableObject = raycastHit.transform.gameObject;
+                Material objectMaterial = pickuppableObject.GetComponent<Renderer>().material;
+                objectMaterial.color = objectHighlightColor;
+
+                // unhighlight object previously looking at
+                Material previousObjectMaterial = currentPickupabbleLookingAt.GetComponent<Renderer>().material;
+                previousObjectMaterial.color = Color.white;
+
+                // make new object currently looked at object
+                currentPickupabbleLookingAt = pickuppableObject;
+            }
+        }
+        // no longer looking at a pickuppable object
+        else
+		{
+            // unhighlight whatever we were previously looking at (if anything)
+            if (currentPickupabbleLookingAt != null)
+			{
+                Material previousObjectMaterial = currentPickupabbleLookingAt.GetComponent<Renderer>().material;
+                previousObjectMaterial.color = Color.white;
+                currentPickupabbleLookingAt = null;
+			}
+		}
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // holding an object
         if (heldObject != null)
 		{
             HoldObject();
         }
+        // check to see if looking at object that could be held
+        else
+        {
+            LookForPickuppableObjects();
+        }
+        // trying to hold an object
         if (Input.GetKeyDown(pickUpButtonController) || Input.GetKeyDown(pickUpButtonKeyboard))
 		{
             if (heldObject == null)

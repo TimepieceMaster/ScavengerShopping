093edf04a1e4b127fdd26a3049b6be99eb282560
TimepieceMaster <JacobVillarrealTP@gmail.com>
2020-11-08 14:52:43 -0600

    Shopping Cart Now Knows What's In It

---
+++ Assets/Scripts/InsideCartTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsideCartTrigger : MonoBehaviour
+{
+	List<UnityEngine.Collider> objectsInCart;
+
+	private void Start()
+	{
+		objectsInCart = new List<UnityEngine.Collider>();
+	}
+
+	private void Update()
+	{
+		objectsInCart.RemoveAll(ColliderDisabled);
+	}
+
+	private static bool ColliderDisabled(UnityEngine.Collider c)
+	{
+		return !c.enabled;
+	}
+
+	private void OnTriggerEnter(UnityEngine.Collider other)
+	{
+		objectsInCart.Add(other);
+	}
+
+	private void OnTriggerExit(UnityEngine.Collider other)
+	{
+		objectsInCart.Remove(other);
+	}
+}

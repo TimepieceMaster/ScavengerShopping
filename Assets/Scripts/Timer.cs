9fcfe3120ca046ea10ceaf219d257f2f40ae02e0
Edward <edward161514@gmail.com>
2020-11-15 14:37:39 -0600

    Sync Menu and Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,23 +3,51 @@ using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
 
 public class Timer : MonoBehaviour
 {
 
+    bool startTimer = false;
+    double timerIncrementValue;
+    double startTime;
+    ExitGames.Client.Photon.Hashtable CustomeValue;
     public Text T;
     public float seconds, minutes;
+
     // Start is called before the first frame update
     void Start()
     {
         T = GetComponent<Text>() as Text;
+
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+         {
+             CustomeValue = new ExitGames.Client.Photon.Hashtable();
+             startTime = PhotonNetwork.Time;
+             startTimer = true;
+             CustomeValue.Add("StartTime", startTime);
+             PhotonNetwork.CurrentRoom.SetCustomProperties(CustomeValue);
+         }
+         else
+         {
+             startTime = double.Parse(PhotonNetwork.CurrentRoom.CustomProperties["StartTime"].ToString());
+             startTimer = true;
+         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!startTimer) return;
+
+        timerIncrementValue = PhotonNetwork.Time - startTime;
+
+        T.text = timerIncrementValue.ToString("00");
+        /*
+
         minutes = (int)(Time.time / 60f);
         seconds = (int)(Time.time % 60f);
-        T.text = minutes.ToString("00") + ":" + seconds.ToString("00");
-            }
+        T.text = minutes.ToString("00") + ":" + seconds.ToString("00");*/
+    }
 }
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
 
 public class UI : MonoBehaviour
 {
@@ -15,7 +17,6 @@ public class UI : MonoBehaviour
     public Text Ing6;
     public Text BonusSushi;
 
-    bool fin = false;
     string[,] MenuList = new string[10, 7] {{"Pancake","Egg","Flour","Milk","Sugar","Whipped Cream","Chocolate Syrup"},
                                       {"Chicken Soup","Chicken","Soup","Rice ","Salt","Salt Crackers","Sugar"},
                                       {"Bread","Bread","Jam","Cheese","Mustard","Mayonnaise","Peanut Butter"},
@@ -28,22 +29,35 @@ public class UI : MonoBehaviour
                                       {"BBQ Party","Cutlet","Candy Bar","Lolipop","Potato Chips","Bubblegum","Creamy Buns" }};
     string[] Sushi = new string[5] {"Chutoro Sushi","Ebi Sushi","Nigiri Sushi","Piece Sushi","Tamagoyaki Sushi"};
 
+    void Start() {
 
-    void Update()
-    {
-        int num = Random.Range(1, 11);
-        int bonus = Random.Range(1, 6);
-        if (!fin)
+        int num = Random.Range(0, 9);
+        int bonus = Random.Range(0, 4);
+
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
         {
-            Menu.text = MenuList[num,0];
-            Ing1.text = MenuList[num,1];
-            Ing2.text = MenuList[num,2];
-            Ing3.text = MenuList[num,3];
-            Ing4.text = MenuList[num,4];
-            Ing5.text = MenuList[num,5];
-            Ing6.text = MenuList[num,6];
-            BonusSushi.text = Sushi[bonus];
-            fin = true;
+            ExitGames.Client.Photon.Hashtable CustomeValue = new ExitGames.Client.Photon.Hashtable();
+            CustomeValue.Add("num", num);
+            CustomeValue.Add("bonus", bonus);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(CustomeValue);
         }
+        else {
+            num = int.Parse(PhotonNetwork.CurrentRoom.CustomProperties["num"].ToString());
+            bonus = int.Parse(PhotonNetwork.CurrentRoom.CustomProperties["bonus"].ToString());
+        }
+
+        SetMenuText(num, bonus);
+
+    }
+
+    void SetMenuText(int num,int bonus) {
+        Menu.text = MenuList[num,0];
+        Ing1.text = MenuList[num,1];
+        Ing2.text = MenuList[num,2];
+        Ing3.text = MenuList[num,3];
+        Ing4.text = MenuList[num,4];
+        Ing5.text = MenuList[num,5];
+        Ing6.text = MenuList[num,6];
+        BonusSushi.text = Sushi[bonus];
     }
 }
